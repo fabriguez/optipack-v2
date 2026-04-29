4dccb2832aed880ff2940e826af4ebf2b6b4e6c9
Brightky Efoo <brightefoo@gmail.com>
2026-04-29 23:57:30 +0100

    removev migrations

--- apps/api/prisma/migrations/20260419000000_drop_warehouse_type_capacity/migration.sql
+++
@@ -1,7 +0,0 @@
--- DropColumn
-ALTER TABLE "warehouses" DROP COLUMN "type",
-DROP COLUMN "maxCapacity",
-DROP COLUMN "currentOccupancy";
-
--- DropEnum
-DROP TYPE "WarehouseType";
--- apps/api/prisma/migrations/20260420000000_parcel_warehouse_entered_at/migration.sql
+++
@@ -1,5 +0,0 @@
--- AddColumn
-ALTER TABLE "parcels" ADD COLUMN "warehouseEnteredAt" TIMESTAMP(3);
-
--- Backfill existing parcels currently in a warehouse
-UPDATE "parcels" SET "warehouseEnteredAt" = "createdAt" WHERE "warehouseId" IS NOT NULL;
